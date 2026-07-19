e62b53e639ec6a5c5ab9526aad3e57929c6695e7
Ezrea7 <ezrea7@gmail.com>
2026-07-19 16:10:39 +0800

    更新 Get_File.py

--- Rules_Actions/Script/Get_File.py
+++ Rules_Actions/Script/Get_File.py
@@ -5,7 +5,7 @@ import time
 import requests
 
 print (os.getcwd())
-os.chdir("./Rules_Actions")
+os.chdir("./rule")
 
 RULE_URL = "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/"
 REJECT_RULES = {
